1ac1f99967b20582ee0a512be16511b5b44d4e24
Sacha Cantat <scantat01@gmail.com>
2023-01-05 10:28:32 +0100

    Ajout fichier Zigbee

---
+++ Batonome-embedded/Core/Inc/zigbee.h
@@ -0,0 +1,13 @@
+/*
+ * zigbee.h
+ *
+ *  Created on: 5 janv. 2023
+ *      Author: scant
+ */
+
+#ifndef INC_ZIGBEE_H_
+#define INC_ZIGBEE_H_
+
+
+
+#endif /* INC_ZIGBEE_H_ */
---
+++ Batonome-embedded/Core/Src/zigbee.c
@@ -0,0 +1,10 @@
+/*
+ * zigbee.c
+ *
+ *  Created on: 5 janv. 2023
+ *      Author: scant
+ */
+
+#include "zigbee.h"
+
+
